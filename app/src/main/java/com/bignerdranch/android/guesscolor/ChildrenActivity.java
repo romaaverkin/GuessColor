2a023e49fb50873abdc6552a58540c582e3b03d0
Roman Averkin <romaaverkin@gmail.com>
2017-07-25 11:35:20 +0700

    Первый комит. Первичное представление. Детская активность.

---
+++ app/src/main/java/com/bignerdranch/android/guesscolor/ChildrenActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.guesscolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChildrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children);
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/guesscolor/GuessActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.guesscolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess);
+    }
+}
